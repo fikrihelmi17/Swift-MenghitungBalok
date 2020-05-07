f9aecdc6d8661e5bdab75a00a63b6b9c134faeb6
fikri <fikrihelmi17@gmail.com>
2020-05-07 15:42:37 +0700

    Initial Commit

---
+++ MenghitungBalok/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MenghitungBalok
+//
+//  Created by Fikri on 07/05/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
